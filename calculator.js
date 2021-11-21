81407f878d6ff4f18275a960662e3e920c342a4f
simeonbain <bainsimeon@gmail.com>
2021-11-21 23:06:15 +1100

    Create a function operate that takes an operator and 2 numbers and then calls one of the basic math operation functions on the numbers

--- calculator.js
+++ calculator.js
@@ -14,3 +14,23 @@ function divide(num1, num2) {
   return num1 / num2; 
 }
 
+function operate(num1, num2, operator) {
+  num1 = +num1; 
+  num2 = +num2; 
+  operator = String(operator);
+
+  switch (operator) {
+    case '+': 
+      return add(num1, num2); 
+    case '-': 
+      return subtract(num1, num2); 
+    case `x`:
+      return multiply(num1, num2);
+    case `/`:
+      return divide(num1, num2); 
+    default: 
+      console.log(`ERROR: Unexpected operator`); 
+  }
+  return; 
+}
+
